5712e51041dad47dd5715ab39ee2f818455a5a09
tranviet191999 <tranviet191999@users.noreply.github.com>
2020-10-29 15:27:51 +0700

    database

--- ci4_cms1.sql
+++ ci4_cms1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 27, 2020 at 10:52 AM
+-- Generation Time: Oct 29, 2020 at 11:26 AM
 -- Server version: 5.6.47
 -- PHP Version: 7.2.29
 
@@ -24,133 +24,108 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `color`
+-- Table structure for table `attribute`
 --
 
-CREATE TABLE `color` (
+CREATE TABLE `attribute` (
   `id` int(11) NOT NULL,
-  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `language` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `catalogueid` int(11) NOT NULL,
+  `catalogue` longtext NOT NULL,
+  `order` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
-  `userid_updated` int(11) NOT NULL,
-  `userid_created` int(11) NOT NULL,
   `updated_at` datetime NOT NULL,
-  `deleted_at` tinyint(4) NOT NULL,
-  `publish` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `deleted_at` tinyint(1) NOT NULL,
+  `publish` tinyint(1) NOT NULL,
+  `userid_created` int(11) NOT NULL,
+  `userid_updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `color`
+-- Dumping data for table `attribute`
 --
 
-INSERT INTO `color` (`id`, `title`, `language`, `code`, `created_at`, `userid_updated`, `userid_created`, `updated_at`, `deleted_at`, `publish`) VALUES
-(39, 'đen', 'vi', '000000', '2020-10-26 16:15:36', 0, 8, '0000-00-00 00:00:00', 0, 1),
-(40, '', 'en', '000000', '2020-10-26 16:15:36', 0, 8, '0000-00-00 00:00:00', 0, 1),
-(41, '', 'jp', '000000', '2020-10-26 16:15:36', 0, 8, '0000-00-00 00:00:00', 0, 1);
+INSERT INTO `attribute` (`id`, `catalogueid`, `catalogue`, `order`, `created_at`, `updated_at`, `deleted_at`, `publish`, `userid_created`, `userid_updated`) VALUES
+(1, 6, 'null', 0, '2020-10-28 16:35:35', '2020-10-28 16:55:34', 0, 1, 8, 8),
+(2, 4, 'null', 0, '2020-10-28 17:07:17', '0000-00-00 00:00:00', 0, 1, 8, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `property`
+-- Table structure for table `attribute_catalogue`
 --
 
-CREATE TABLE `property` (
+CREATE TABLE `attribute_catalogue` (
   `id` int(11) NOT NULL,
   `parentid` int(11) NOT NULL,
+  `lft` int(11) NOT NULL,
+  `rgt` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
+  `deleted_at` tinyint(1) NOT NULL,
+  `publish` tinyint(1) NOT NULL,
   `userid_created` int(11) NOT NULL,
-  `userid_updated` int(11) NOT NULL,
-  `deleted_at` tinyint(4) NOT NULL,
-  `publish` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `userid_updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `property`
+-- Dumping data for table `attribute_catalogue`
 --
 
-INSERT INTO `property` (`id`, `parentid`, `created_at`, `updated_at`, `userid_created`, `userid_updated`, `deleted_at`, `publish`) VALUES
-(6, 9, '2020-10-27 12:41:36', '0000-00-00 00:00:00', 8, 0, 0, 1),
-(7, 9, '2020-10-27 12:42:17', '0000-00-00 00:00:00', 8, 0, 0, 1),
-(8, 9, '2020-10-27 12:42:45', '2020-10-27 14:35:39', 8, 8, 1, 1),
-(9, 11, '2020-10-27 14:25:50', '0000-00-00 00:00:00', 8, 0, 0, 1);
+INSERT INTO `attribute_catalogue` (`id`, `parentid`, `lft`, `rgt`, `level`, `order`, `created_at`, `updated_at`, `deleted_at`, `publish`, `userid_created`, `userid_updated`) VALUES
+(2, 0, 0, 0, 0, 0, '2020-10-28 15:31:52', '0000-00-00 00:00:00', 0, 1, 8, 0),
+(3, 0, 2, 3, 1, 0, '2020-10-28 15:32:23', '2020-10-28 15:48:38', 0, 1, 8, 8),
+(4, 0, 4, 9, 1, 0, '2020-10-28 15:35:51', '0000-00-00 00:00:00', 0, 1, 8, 0),
+(5, 4, 5, 8, 0, 0, '2020-10-28 15:43:12', '0000-00-00 00:00:00', 0, 1, 8, 0),
+(6, 5, 6, 7, 3, 0, '2020-10-28 15:43:41', '0000-00-00 00:00:00', 0, 1, 8, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `property_catalogue`
+-- Table structure for table `attribute_translate`
 --
 
-CREATE TABLE `property_catalogue` (
-  `id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` int(11) NOT NULL,
-  `userid_created` int(11) NOT NULL,
-  `userid-updated` int(11) NOT NULL,
-  `deleted_at` tinyint(4) NOT NULL,
-  `publish` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `property_catalogue`
---
-
-INSERT INTO `property_catalogue` (`id`, `created_at`, `updated_at`, `userid_created`, `userid-updated`, `deleted_at`, `publish`) VALUES
-(9, '2020-10-26 17:13:05', 0, 8, 0, 0, 1),
-(10, '2020-10-26 17:14:30', 0, 8, 0, 0, 1),
-(11, '2020-10-26 17:15:16', 0, 8, 0, 0, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `property_translate`
---
-
-CREATE TABLE `property_translate` (
-  `module` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE `attribute_translate` (
   `objectid` int(11) NOT NULL,
-  `language` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `language` longtext NOT NULL,
+  `module` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `value` varchar(20) NOT NULL,
+  `userid_created` int(11) NOT NULL,
+  `userid_updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `property_translate`
+-- Dumping data for table `attribute_translate`
 --
 
-INSERT INTO `property_translate` (`module`, `title`, `value`, `objectid`, `language`) VALUES
-('property_catalogue', 'kích thước', '', 9, 'vi'),
-('property_catalogue', 'phụ kiện', '', 10, 'vi'),
-('property_catalogue', 'ram', '', 11, 'vi'),
-('property_catalogue', 'ram', '', 11, 'en'),
-('property_catalogue', 'phụ kiện', '', 10, 'en'),
-('property', 'Size', 'M', 6, 'vi'),
-('property', 'Size', 'S', 7, 'vi'),
-('property', 'Size', 'XXL', 8, 'vi'),
-('property', 'Ram ', '8gb', 9, 'vi'),
-('property', 'Kích thước', '', 7, 'en');
+INSERT INTO `attribute_translate` (`objectid`, `language`, `module`, `title`, `value`, `userid_created`, `userid_updated`) VALUES
+(3, 'vi', 'attribute_catalogue', 'Kích thước 111', '', 0, 0),
+(4, 'vi', 'attribute_catalogue', 'Máy tính', '', 0, 0),
+(5, 'vi', 'attribute_catalogue', 'Phụ kiện máy tính', '', 0, 0),
+(6, 'vi', 'attribute_catalogue', 'Tai nghe', '', 0, 0),
+(3, 'en', 'attribute_catalogue', 'kích thước', '', 0, 0),
+(3, 'jp', 'attribute_catalogue', 'abs', '', 0, 0),
+(1, 'vi', 'attribute', 'admin', 'SSSSS', 0, 0),
+(1, 'en', 'attribute', 'admin', 'SSSS', 0, 0),
+(2, 'vi', 'attribute', 'Màu sắc', 'Đỏ', 0, 0),
+(2, 'en', 'attribute', 'color', 'red', 0, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `color`
+-- Indexes for table `attribute`
 --
-ALTER TABLE `color`
+ALTER TABLE `attribute`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `property`
+-- Indexes for table `attribute_catalogue`
 --
-ALTER TABLE `property`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `property_catalogue`
---
-ALTER TABLE `property_catalogue`
+ALTER TABLE `attribute_catalogue`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -158,22 +133,16 @@ ALTER TABLE `property_catalogue`
 --
 
 --
--- AUTO_INCREMENT for table `color`
+-- AUTO_INCREMENT for table `attribute`
 --
-ALTER TABLE `color`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+ALTER TABLE `attribute`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `property`
+-- AUTO_INCREMENT for table `attribute_catalogue`
 --
-ALTER TABLE `property`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `property_catalogue`
---
-ALTER TABLE `property_catalogue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `attribute_catalogue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
